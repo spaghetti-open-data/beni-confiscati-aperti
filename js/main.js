1cb34a783c61c1f82066991a68e094e4f8d5d864
maccaluso <Macpro@Mac-Pro.local>
2014-04-10 04:03:22 +0200

    Layout iniziale, stili e interfaccia

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+var BC = {
 
+	//menuOpen: false,
+	init: function(){},
+	registerUI: function(){
+
+		$('body').on('click', '#toggle-menu', function(e){
+			var destR = 0;
+			$('#menu').animate({ right: destR },100)
+		});
+		$('body').on('mouseleave', '#menu', function(e){
+			var destR = '-20%';
+			$('#menu').animate({ right: destR },100)
+		});
+
+	}
+
+}
+
+$(document).ready(function(){
+
+	BC.registerUI();
+
+});
